cdcb039dab059b1a4bcbdc3f9fc195a74be07d2f
hyunjin <alfma123@naver.com>
2023-11-13 12:54:31 +0900

    feat: 하위 도메인 라우팅

--- README.md
+++ README.md
@@ -9,3 +9,27 @@
     }
   }
 ```
+
+## 라우트 매개변수
+
+```ts
+ @Delete(':userId/memo/:memoId')
+  deleteUserMemo(
+    @Param('userId') userId: string,
+    @Param('memoId') memoId: string,
+  ) {
+    return `userId: ${userId}, memoId: ${memoId}`;
+  }
+```
+
+## 하위 도메인 라우팅
+
+```ts
+@Controller({ host: 'api.localhost' })
+export class ApiController {
+  @Get()
+  index(): string {
+    return 'hello API';
+  }
+}
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ApiController } from './api/api.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
+  controllers: [ApiController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
