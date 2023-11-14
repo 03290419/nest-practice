a4b34b561f2807fb0c3873c7894a36a671b432ab
hyunjin <alfma123@naver.com>
2023-11-14 22:48:39 +0900

    feat: dotenv 직접 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { AppModule } from './app.module';
 
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : process.env.NODE_ENV === 'stage'
+      ? '.stage.env'
+      : '.development.env',
+  ),
+});
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
