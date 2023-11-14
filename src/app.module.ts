1e4096c3c72d5504a4e68cc40dfeac6750452f4c
hyunjin <alfma123@naver.com>
2023-11-14 15:29:01 +0900

    feat: UserProvider 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailService } from './email/email.service';
 import { UsersController } from './users/users.controller';
+import { UserService } from './users/users.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UserService, EmailService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,31 +3,32 @@ import { UserInfo } from './UserInfo';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UserService) {}
+
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    console.log(dto);
+    const { name, email, password } = dto;
+    return await this.userService.createUser(name, email, password);
   }
 
   @Post('/email-verify')
-  async verifyEmail(
-    @Query() dto: VerifyEmailDto,
-  ): Promise<{ [key: string]: string }> {
-    console.log(dto);
-    return { message: 'email' };
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
+    return await this.userService.verifyEmail(signupVerifyToken);
   }
 
   @Post('/login')
-  async login(@Body() dto: UserLoginDto): Promise<{ [key: string]: string }> {
-    console.log(dto);
-    return { message: 'login' };
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+    return await this.userService.login(email, password);
   }
 
   @Get(':id')
   async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
-    console.log(userId);
-    return { id: 'keng', name: 'kengsuke', email: 'keng@gmail.com' };
+    return this.userService.getUserInfo(userId);
   }
 }
